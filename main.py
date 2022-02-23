477910fabdde84af89fa49bcaa67f2ddf6b1fc92
Emilio <milosagastume11@gmail.com>
2022-02-23 12:00:13 -0600

    Configuracion flask + mariadb (docker)

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
 #Importamos flask
-from crypt import methods
+import json
+
 import flask
+import mariadb
 
 #Creamos la app de flask
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route('/')
-def hola_ubuntulog():
-    return 'Succes!'
+# configuration used to connect to MariaDB
+config = {
+    'host': '127.0.0.1',
+    'port': 3306,
+    'user': 'milosagasdc11',
+    'password': 'Milosqui11',
+    'database': 'personas'
+}
+
+
+# route to return all people
+@app.route('/api/people', methods=['GET'])
+def index():
+   # connection for MariaDB
+   conn = mariadb.connect(**config)
+   # create a connection cursor
+   cur = conn.cursor()
+   # execute a SQL statement
+   cur.execute("select * from persona")
+
+   # serialize results into JSON
+   row_headers=[x[0] for x in cur.description]
+   rv = cur.fetchall()
+   json_data=[]
+   for result in rv:
+        json_data.append(dict(zip(row_headers,result)))
+
+   # return the results!
+   return json.dumps(json_data)
 
 #run de appp
 app.run()
-    
     
