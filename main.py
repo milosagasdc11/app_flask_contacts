ec80ea4880a77cd648bb0bd04238b7c5acf74f73
Emilio <milosagastume11@gmail.com>
2022-02-27 16:36:55 -0600

    Crud listo

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-#Importamos flask
+#Importamos flask, mariadb y render
+from base64 import urlsafe_b64decode
+from crypt import methods
 import json
-
 import flask
+from flask import render_template, request, redirect,  url_for, flash
 import mariadb
+
+
 
 #Creamos la app de flask
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+
+#Inicializamos la sesion
+app.secret_key= 'mysecretkey'
 
 # configuration used to connect to MariaDB
 config = {
@@ -14,29 +21,87 @@ config = {
     'port': 3306,
     'user': 'milosagasdc11',
     'password': 'Milosqui11',
-    'database': 'personas'
+    'database': 'flask_contacts'
 }
 
+# metodos para el enrutamiento
+@app.route('/')
+def index(): 
+    data = select_all_contacts()
+    return render_template('index.html', contacts = data) #Direcciono al index y le envio los datos
 
-# route to return all people
-@app.route('/api/people', methods=['GET'])
-def index():
-   # connection for MariaDB
-   conn = mariadb.connect(**config)
-   # create a connection cursor
-   cur = conn.cursor()
-   # execute a SQL statement
-   cur.execute("select * from persona")
 
-   # serialize results into JSON
-   row_headers=[x[0] for x in cur.description]
-   rv = cur.fetchall()
-   json_data=[]
-   for result in rv:
-        json_data.append(dict(zip(row_headers,result)))
+@app.route('/add', methods=['POST'])
+def add_contact():
+    if request.method == 'POST':
+        fullname = request.form['fullname']
+        phone = request.form['phone']
+        email = request.form['email']
+        insert_contact(fullname, phone, email)
+        return redirect (url_for('index'))
+        
 
-   # return the results!
-   return json.dumps(json_data)
+@app.route('/edit/<id>')
+def edit_contact(id):
+    data = select_contact_byId(id)
+    return render_template('edit_contact.html', contact = data[0])
+
+@app.route('/delete/<id>')
+def delete_contact(id):
+    delete_contact_byId(id)
+    return redirect(url_for('index'))
+
+
+@app.route('/update/<id>', methods = ['POST'])
+def update_contact(id):
+    if request.method == 'POST':
+        fullname = request.form['fullname']
+        phone = request.form['phone']
+        email = request.form['email']
+        update_contact(id,fullname, phone, email)
+        return redirect(url_for('index'))
+
+
+#mariadb contacts  
+def insert_contact(fullname, phone, email):
+    conn = mariadb.connect(**config)
+    cur = conn.cursor()
+    cur.execute("INSERT INTO contacts (fullname, phone, email) VALUES(%s, %s, %s)", 
+    (fullname, phone, email))
+    conn.commit()
+    print('Contacto insertado')
+
+def select_all_contacts():
+    conn = mariadb.connect(**config)
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM contacts")
+    data = cur.fetchall()
+    return data
+
+def delete_contact_byId(id):
+    conn = mariadb.connect(**config)
+    cur = conn.cursor()
+    cur.execute("DELETE FROM contacts where id = {0}".format(id))
+    conn.commit()
+    print('Contacto eliminado')
+
+def select_contact_byId(id):
+    conn = mariadb.connect(**config)
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM contacts where id = {0}".format(id))
+    data = cur.fetchall()
+    return data
+
+def update_contact(id, fullname, phone, email):
+    conn = mariadb.connect(**config)
+    cur = conn.cursor()
+    cur.execute("UPDATE contacts SET fullname = %s, phone=%s, email = %s WHERE id = %s", 
+    (fullname, phone, email, id))
+    conn.commit()
+    print('Actualizado con exito')
+
+
+
 
 #run de appp
 app.run()
